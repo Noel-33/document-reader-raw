be771d00d6d6dbf49fdbe1ed7dfcf1ce63d1d163
Noel-33 <noelneelankavil@gmail.com>
2026-03-14 19:40:45 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ def parse_allowed_origins() -> List[str]:
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://index-8c1d0.web.app",
-        "https://index-8c1d0.firebaseapp.com"
+        "https://index-8c1d0.firebaseapp.com",
         *configured_urls,
     ]
 
